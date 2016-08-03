70ffe5d58b3d0617800e4c54cc008d02eae38ca2
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-08-03 13:24:46 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- Command/StartScheduledWorkerCommand.php
+++ Command/StartScheduledWorkerCommand.php
@@ -106,7 +106,7 @@ class StartScheduledWorkerCommand extends ContainerAwareCommand
         $output->writeln(\sprintf('Starting worker <info>%s</info>', $process->getCommandLine()));
 
         if ($input->getOption('foreground')) {
-            $process->run(function ($type, $buffer) use ($output) {
+            $process->run(function($type, $buffer) use ($output) {
                 $output->write($buffer);
             });
         } // else we recompose and display the worker id
